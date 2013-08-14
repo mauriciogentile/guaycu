a855ba47040832458cc99f319ce4f196b4d8e3f3
Mauricio Gentile <gentilemauricio@gmail.com>
2013-08-14 12:30:10 -0400

    Adding mongo models.

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,10 @@ var navigation = require("../controllers/navigation");
 
 module.exports = function(app) {
     app.get("/", navigation.home);
-    app.get("/about", navigation.about);
+    app.get("/twitter", navigation.twitter);
     app.get("/work", navigation.work);
     app.get("/services", navigation.services);
     app.get("/blog", navigation.home);
     app.get("/contact", navigation.home);
+    app.get("/paper", navigation.paper);
 };
