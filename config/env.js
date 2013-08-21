ac032512d4cfb15726f6b2697c451bd93ea828ec
Mauricio Gentile <gentilemauricio@gmail.com>
2013-08-20 22:42:07 -0400

    Adding edit view.

--- config/env.js
+++ config/env.js
@@ -2,5 +2,6 @@ var env = process.env;
 
 module.exports = {
   //mongoUrl: env.MONGOHQ_URL
-  mongoUrl: "mongodb://localhost:27017/guaycu"
+  mongoUrl: "mongodb://localhost:27017/guaycu",
+  port: 8080
 };
