ff9bea77117e41f472cc67a2175d6b3a3232d83e
Mauricio Gentile <gentilemauricio@gmail.com>
2013-09-28 13:55:28 -0400

    changing bg

--- config/env.js
+++ config/env.js
@@ -2,6 +2,6 @@ module.exports = (function() {
     return {
 		mongoUrl: process.env.MONGOHQ_URL || "mongodb://localhost:27017/guaycu",
 	  	port: process.env.PORT || 8080,
-	  	ip: "127.0.0.1"
+	  	ip: "localhost"
 	};
 })();
