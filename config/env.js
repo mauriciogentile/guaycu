b9ae358008ff3c0cdfda471e536c6aaed5360ff3
Mauricio Gentile <gentilemauricio@gmail.com>
2013-10-09 11:34:27 -0400

    afd

--- config/env.js
+++ config/env.js
@@ -1,7 +1,7 @@
 module.exports = (function() {
     return {
 		mongoUrl: process.env.MONGOHQ_URL || "mongodb://localhost:27017/guaycu",
-	  	port: process.env.PORT || 8080,
+	  	port: process.env.PORT || 8082,
 	  	ip: "localhost"
 	};
 })();
